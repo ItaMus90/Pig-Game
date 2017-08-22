f41dae87761b6ba64cce071cd6f825381400e201
Itamar <itamarmuslvi@gmail.com>
2017-08-22 09:21:46 +0300

    Add input text in html for give the players the option to determine the result target

--- resources/js/appWithNewFeature.js
+++ resources/js/appWithNewFeature.js
@@ -98,9 +98,20 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 
         //Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
+        
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        
+        // Undefined, 0, null, or "" are Coerced to false
+        //And Anything else is Coerced to true
+        if(!isNaN(input)){
+            winningScore = input; 
+        }else{
+            winningScore = 100;
+        }
+         
         //Cheack if player won the game 
-        if(scores[activePlayer] >= 100){
+        if(scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
